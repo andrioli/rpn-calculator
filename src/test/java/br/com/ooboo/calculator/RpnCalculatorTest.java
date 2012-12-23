a0f1313eaea4853f93614303eef6650b2bb69b6a
Roberto Araujo <roberto.andrioli@gmail.com>
2012-12-23 21:08:49 -0200

    Refactoring test

--- src/test/java/br/com/ooboo/calculator/RpnCalculatorTest.java
+++ src/test/java/br/com/ooboo/calculator/RpnCalculatorTest.java
@@ -3,20 +3,26 @@ package br.com.ooboo.calculator;
 import java.math.BigDecimal;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class RpnCalculatorTest {
+
+	private RpnCalculator calculator;
+
+	@Before
+	public void init() {
+		calculator = new RpnCalculator();
+	}
 	
 	@Test
 	public void NewCalculatorHas0InItsAccumulator() {
-		RpnCalculator calculator = new RpnCalculator();
 		Assert.assertEquals(BigDecimal.ZERO, calculator.getAccumulator());
 	}
 	
 	@Test
 	public void NewCalculatorShouldAllowItsAccumulatorToBeSet() {
 		BigDecimal value = new BigDecimal(42);
-		RpnCalculator calculator = new RpnCalculator();
 		calculator.setAccumulator(value);
 		Assert.assertEquals(value, calculator.getAccumulator());
 	}
