c2ab756b770d733192b88fd4ef893ca4a84c6c77
Roberto Araujo <roberto.andrioli@gmail.com>
2012-12-23 21:24:49 -0200

    A calculator should allow multiple values to be stored

--- src/main/java/br/com/ooboo/calculator/RpnCalculator.java
+++ src/main/java/br/com/ooboo/calculator/RpnCalculator.java
@@ -1,8 +1,11 @@
 package br.com.ooboo.calculator;
 
 import java.math.BigDecimal;
+import java.util.Stack;
 
 public class RpnCalculator {
+	
+	private Stack<BigDecimal> stack = new Stack<>();
 
 	private BigDecimal accumulator = BigDecimal.ZERO;
 
@@ -12,6 +15,17 @@ public class RpnCalculator {
 
 	public void setAccumulator(BigDecimal value) {
 		accumulator = value;
+	}
+
+	public void enter() {
+		stack.push(accumulator);
+	}
+
+	public void drop() {
+		if (!stack.isEmpty())
+			accumulator = stack.pop();
+		else
+			accumulator = BigDecimal.ZERO;
 	}
 	
 }
--- src/test/java/br/com/ooboo/calculator/ANewlyCreateRpnCalculatorShould.java
+++ src/test/java/br/com/ooboo/calculator/ANewlyCreateRpnCalculatorShould.java
@@ -26,5 +26,24 @@ public class ANewlyCreateRpnCalculatorShould {
 		calculator.setAccumulator(value);
 		Assert.assertEquals(value, calculator.getAccumulator());
 	}
+	
+	@Test
+	public void AllowMultipleValuesToBeStored() {
+		BigDecimal value1 = new BigDecimal(42);
+		BigDecimal value2 = new BigDecimal(2);
+		BigDecimal value3 = new BigDecimal(3);
+		calculator.setAccumulator(value1);
+		calculator.enter();
+		calculator.setAccumulator(value2);
+		calculator.enter();
+		calculator.setAccumulator(value3);
+		Assert.assertEquals(value3, calculator.getAccumulator());
+		calculator.drop();
+		Assert.assertEquals(value2, calculator.getAccumulator());
+		calculator.drop();
+		Assert.assertEquals(value1, calculator.getAccumulator());
+		calculator.drop();
+		Assert.assertEquals(BigDecimal.ZERO, calculator.getAccumulator());
+	}
 
 }
