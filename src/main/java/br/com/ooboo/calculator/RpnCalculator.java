d0310150a1defa42b72a8a47cfd084bb4eef840f
Roberto Araujo <roberto.andrioli@gmail.com>
2012-12-23 21:01:54 -0200

    A new calculator has 0 in its accumulator

---
+++ src/main/java/br/com/ooboo/calculator/RpnCalculator.java
@@ -0,0 +1,11 @@
+package br.com.ooboo.calculator;
+
+import java.math.BigDecimal;
+
+public class RpnCalculator {
+
+	public BigDecimal getAccumulator() {
+		return BigDecimal.ZERO;
+	}
+	
+}
--- src/test/java/br/com/ooboo/calculator/RpnCalculatorTest.java
+++ src/test/java/br/com/ooboo/calculator/RpnCalculatorTest.java
@@ -1,12 +1,16 @@
 package br.com.ooboo.calculator;
 
+import java.math.BigDecimal;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class RpnCalculatorTest {
 	
 	@Test
-	public void foo() {
-		
+	public void NewCalculatorHas0InItsAccumulator() {
+		RpnCalculator calculator = new RpnCalculator();
+		Assert.assertEquals(BigDecimal.ZERO, calculator.getAccumulator());
 	}
-
+	
 }
