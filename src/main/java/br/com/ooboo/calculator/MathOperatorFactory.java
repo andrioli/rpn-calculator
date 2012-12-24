28d0b06d3e66a564cbebd889601b5ed3979c6765
Roberto Araujo <roberto.andrioli@gmail.com>
2012-12-24 03:07:07 -0200

    Changing MathOperatorFactory implementation

--- src/main/java/br/com/ooboo/calculator/MathOperatorFactory.java
+++ src/main/java/br/com/ooboo/calculator/MathOperatorFactory.java
@@ -1,5 +1,8 @@
 package br.com.ooboo.calculator;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import br.com.ooboo.calculator.operator.Add;
 import br.com.ooboo.calculator.operator.Divide;
 import br.com.ooboo.calculator.operator.Factorial;
@@ -10,23 +13,29 @@ import br.com.ooboo.calculator.operator.Subtract;
 
 public class MathOperatorFactory {
 	
+	private Map<String, MathOperator> operatorsByName;
+	
+	public MathOperatorFactory() {
+		operatorsByName = new ConcurrentHashMap<>();
+		registerOperators();
+	}
+	
+	private void registerOperators() {
+		operatorsByName.put("+", new Add());
+		operatorsByName.put("-", new Subtract());
+		operatorsByName.put("*", new Multiply());
+		operatorsByName.put("/", new Divide());
+		operatorsByName.put("!", new Factorial());
+	}
+
 	public MathOperator findOperatorNamed(String operatorName) {
-		MathOperator op = null;
+		MathOperator op = operatorsByName.get(operatorName);
 		
-		if ("+".equals(operatorName)) {
-			op = new Add();
-		} else if ("-".equals(operatorName)) {
-			op = new Subtract();
-		} else if ("!".equals(operatorName)) {
-			op = new Factorial();
-		} else if ("*".equals(operatorName)) {
-			op = new Multiply();
-		} else if ("/".equals(operatorName)) {
-			op = new Divide();
-		} else {
+		if (op == null){
 			throw new NoSuchOperatorException(
 					"Invalid operator named: " + operatorName);
 		}
+		
 		return op;
 	}	
 
